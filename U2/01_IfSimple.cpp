4f4aa8dbddec69b3d088a59f302c458913971985
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-08-11 11:28:32 -0500

    If simple

---
+++ U2/01_IfSimple.cpp
@@ -0,0 +1,34 @@
+/* Unidad 2. Uso del if else
+   Autor: Luis Ernesto Anaya Tiscareño
+   Fecha: 01/09/2022
+   Descripción: Muestra el uso del if else simple
+*/
+
+//Libreria para manejo de entradas y salida de pantalla
+#include <iostream>
+
+//Uso del namespace para evitar el std::
+using namespace std;
+
+//Función principal de tipo entero
+int main()
+{
+   int numero;
+   int dato = 1;
+
+   cout << "Ingresa un numero entero: ";
+   cin >> numero;
+   if (numero == dato){
+    cout << "El número es igual a dato" << endl;
+   }   
+   else{
+    cout << "El número es diferente del dato" << endl;
+   }
+
+    //Ejercicio: utilice las diferentes comparaciones ==, !=, <, >, <=, >=
+
+    
+
+    //Al ser una función debe retornar un valor, en este caso 0
+    return 0;
+}
