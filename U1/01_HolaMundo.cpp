fc00c6e07901508cfb7c62ffe8506c315ff11860
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-08-10 19:34:06 -0500

    Comentarios en hola mundo

--- U1/01_HolaMundo.cpp
+++ U1/01_HolaMundo.cpp
@@ -1,8 +1,16 @@
+/* Unidad 1. Hola mundo
+   Autor: Luis Ernesto Anaya Tiscareño
+*/
+
+//Libreria para manejo de entradas y salida de pantalla
 #include <iostream>
 
+//Función principal de tipo entero
 int main()
 {
+    //Mediante la instrucción std::cout envia por la pantalla el texto
     std::cout << "Hola mundo\n";
     
+    //Al ser una función debe retornar un valor, en este caso 0
     return 0;
 }
