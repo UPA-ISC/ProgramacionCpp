2811a44ad66352e134cf02f61d8d2ec3e5d1bd51
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-08-17 18:51:18 -0500

    Variables globales

---
+++ U3/02_VariableGlobal.cpp
@@ -0,0 +1,40 @@
+/* Unidad 3. Variables globales
+   Autor: Luis Ernesto Anaya Tiscareño
+   Fecha: 01/09/2022
+   Descripción: Muestra el alcance de las variables, este es un programa que hace uso de variables globales
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+//Prototipado de funciones
+void getDatos();
+void operacion();
+
+//Variables globales
+float dato1, dato2;
+float resultado;
+
+//Función principal
+int main(){
+
+    getDatos();
+    operacion();
+   
+    cout << "El resultado es: " << resultado << endl;
+
+    return 0;
+}
+
+void getDatos(){
+    cout << "Dame un número: ";
+    cin >> dato1;
+    cout << "Dame otro número: ";
+    cin >> dato2;
+}
+
+void operacion(){
+    resultado = dato1*dato2;
+}
